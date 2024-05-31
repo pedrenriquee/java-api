62a6dc7dadce80cdb3254da1d329e83210143ebb
Pedro Henrique <pedrollopes31@gmail.com>
2024-05-30 22:34:30 -0300

    criando o services para User

---
+++ src/main/java/com/pedrohenrique/todosimple/services/UserService.java
@@ -0,0 +1,55 @@
+package com.pedrohenrique.todosimple.services;
+
+import java.util.Optional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import com.pedrohenrique.todosimple.models.User;
+import com.pedrohenrique.todosimple.repositories.TaskRepository;
+import com.pedrohenrique.todosimple.repositories.UserRespository;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRespository userRespository;
+
+    @Autowired
+    private TaskRepository taskRepository;
+
+    
+    public User findById(Long id) {
+        Optional<User> user = this.userRespository.findById(id);
+        return user.orElseThrow(() -> new RuntimeException(
+            "Usuario não encontrado ! Id: " + id + ", tipo:" + User.class.getName()
+
+        ));
+        
+    }
+
+    @Transactional
+    public User create(User obj, User user) {
+        //caso o jpa não consiga gerar novos ids provalvemente o erro está aqui
+        //user.setId( null);
+        obj = this.userRespository.save(obj);
+        this.taskRepository.saveAll(obj.getTasks());
+        return obj;
+    }
+
+    @Transactional
+    public User update(User obj) {
+        User newObj = findById(obj.getId());
+        newObj.setPassword(obj.getPassword());
+        return this.userRespository.save(newObj);
+
+    }
+
+    public void delete(Long id) {
+        findById(id);
+        try {
+            this.userRespository.deleteById(id);
+        } catch (Exception e) {
+           throw new RuntimeException("Não é possivel excluir pois há entidades relacionadas");
+        }
+    }
+}
