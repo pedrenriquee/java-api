4b24531aa21988c753b9585d048c6e5011c1d83f
Pedro Henrique <pedrollopes31@gmail.com>
2024-06-02 23:36:03 -0300

    Adicionado a TaskController

--- src/main/java/com/pedrohenrique/todosimple/services/TaskService.java
+++ src/main/java/com/pedrohenrique/todosimple/services/TaskService.java
@@ -1,5 +1,6 @@
 package com.pedrohenrique.todosimple.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -29,6 +30,13 @@ public class TaskService {
             
         
     }
+
+    public List<Task> findAllByUserid(Long userid){
+        List<Task> tasks = this.taskRepository.findByUser_id(userid);
+        return tasks;
+        
+    }
+
     @Transactional
     public Task create (Task obj){
         User user = this.userService.findById(obj.getUser().getId());
